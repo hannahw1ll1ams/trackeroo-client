e53d1f4ba10984a6f1261842c965626839558f2f
hannahw1ll1ams <hannahw1ll1ams@yahoo.com>
2019-10-02 16:27:44 +0100

    adding rules for register page

--- Trackeroo/app/screens/Login/LoginScreen.js
+++ Trackeroo/app/screens/Login/LoginScreen.js
@@ -5,6 +5,9 @@ import { Text, View, Button, TextInput } from 'react-native';
 
 
 class LoginScreen extends Component {
+  static navigationOptions = ({ navigation }) => ({
+    headerTitle: "TRACKEROO"
+  });
   state = {
     username: null,
     password: null,
@@ -14,7 +17,6 @@ class LoginScreen extends Component {
 
   handleUsernameChange = (e) => {
     const { text } = e.nativeEvent
-    console.log(e.nativeEvent.text)
     usernameRule = /^[a-zA-Z0-9]+$/
     if (usernameRule.test(text)) {
       console.log('username is valid')
@@ -26,8 +28,6 @@ class LoginScreen extends Component {
 
   handlePasswordChange = (e) => {
     const { text } = e.nativeEvent
-    console.log(e.nativeEvent.text)
-
     passwordRule = /(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?!.*[!?/[/@#"{}()<>£%+='$:;%^&*])(?=.{8,})/;
     if (passwordRule.test(text)) {
       console.log('password is valid')
@@ -37,20 +37,46 @@ class LoginScreen extends Component {
       console.log('password is invalid')
   }
 
+
+  handleChange = (event, inputType) => {
+    const { text } = event.nativeEvent
+    usernameRule = /^[a-zA-Z0-9]+$/
+    passwordRule = /(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?!.*[!?/[/@#"{}()<>£%+='$:;%^&*])(?=.{8,})/;
+    //need to add to check for spaces and improve testing for special characters
+    if (inputType === 'password') {
+      if (passwordRule.test(text)) {
+        console.log('password is valid')
+        this.setState({ [inputType]: text })
+      }
+      else
+        console.log('password is invalid')
+    }
+    if (inputType === 'username') {
+      if (usernameRule.test(text)) {
+        console.log('username is valid')
+        this.setState({ [inputType]: text })
+      }
+      else
+        console.log('username is invalid')
+    }
+
+  }
+
+
   handleSubmit = () => {
     const { username, password, validUser } = this.state
     const { navigate } = this.props.navigation;
-    api.checkExistingUser(username, password).then((boolean) => {
-      this.setState({ validUser: boolean })
-    })
-      .catch(error => {
-        this.setState({
-          error
-        })
-      })
-    if (validUser) {
-      navigate('HomeScreen', { username: username, password: password, otherParam: 'HomePage' })
-    }
+    // api.checkExistingUser(username, password).then((boolean) => {
+    //   this.setState({ validUser: boolean })
+    // })
+    //   .catch(error => {
+    //     this.setState({
+    //       error
+    //     })
+    //   })
+    // if (validUser) {
+    navigate('HomeScreen', { username, password, title: 'Home' })
+    // }
   }
 
   render() {
@@ -59,13 +85,13 @@ class LoginScreen extends Component {
     if (error) return <Text>{error}</Text>
     return (
       <View style={styles.container}>
-        <TextInput placeholder='username' onEndEditing={(event) => this.handleUsernameChange(event)} name='username'
+        <Text> Sign Up</Text>
+        <TextInput placeholder='username' onEndEditing={(event) => this.handleChange(event, 'username')} name='username'
         />
-        <TextInput placeholder='password' name='password' onEndEditing={(event) => this.handlePasswordChange(event)}
+        <TextInput placeholder='password' name='password' onEndEditing={(event) => this.handleChange(event, 'password')}
         />
-        <Text> This is the LoginScreen.</Text>
-        <Button onPress={this.handleSubmit} title="Sign In" />
-        <Button title="Create a account" onPress={() => navigate('RegisterScreen')} />
+        <Button title="Sign In" onPress={this.handleSubmit} />
+        <Button title="Don't have a account?" onPress={() => navigate('RegisterScreen', { title: 'Create a account' })} />
       </View>
     );
   }
