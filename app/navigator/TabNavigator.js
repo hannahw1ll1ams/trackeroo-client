207f87eef6217a14b24fd3369a28110d6ee5284d
Thanh <tmdoan98@gmail.com>
2019-10-05 15:09:22 +0100

    add icons to tab navigation

--- app/navigator/TabNavigator.js
+++ app/navigator/TabNavigator.js
@@ -1,9 +1,10 @@
+import React from "react";
 import HomeScreen from "../screens/Home";
 import MapView from "../screens/MapView";
 import FeedScreen from "../screens/Feed";
 import FollowingScreen from "../screens/Following";
 import RunScreen from "../screens/Run";
-
+import { MaterialIcons } from "@expo/vector-icons";
 import { createBottomTabNavigator } from "react-navigation-tabs";
 import { createStackNavigator } from "react-navigation-stack";
 
@@ -22,17 +23,34 @@ const TabNavigator = createBottomTabNavigator(
   {
     tabBarOptions: {
       style: {
-        backgroundColor: "#121212"
+        backgroundColor: "#121212",
+        height: 52,
+        border: "none"
       },
-      activeTintColor: "#ce93d8"
+      activeTintColor: "#ce93d8",
+      inactiveTintColor: "rgba(255,255,255,0.7)"
     },
-    defaultNavigationOptions: {
-      headerStyle: {},
-      headerTintColor: "#fff",
-      headerTitleStyle: {
-        fontWeight: "bold"
+    defaultNavigationOptions: ({ navigation }) => ({
+      tabBarIcon: ({ focused, horizontal, tintColor }) => {
+        const { routeName } = navigation.state;
+        let IconComponent = MaterialIcons;
+        let iconName;
+
+        if (routeName === "Home") {
+          iconName = "home";
+        }
+        if (routeName === "Run") {
+          iconName = "track-changes";
+        }
+        if (routeName === "Feed") {
+          iconName = "rss-feed";
+        }
+        if (routeName === "Following") {
+          iconName = "people";
+        }
+        return <IconComponent name={iconName} size={24} color={tintColor} />;
       }
-    }
+    })
   }
 );
 
