c77c69be42c55162b3e72333b84a8f4e08d08c6a
Thanh <tmdoan98@gmail.com>
2019-10-05 09:29:48 +0100

    restructure project

--- App.js
+++ App.js
@@ -1,13 +1,13 @@
 import React, { Component } from "react";
 // import Orientation from "react-native-orientation";
 import Navigator from "./app/navigator/Navigator";
-import * as api from './api'
+import * as api from "./api";
 
 export default class App extends Component {
-  // componentDidMount = () => {
-  //   Orientation.lockToPortrait();
-  // };
-
+  componentDidMount = async () => {
+    await api.storeToken("token", "adadd");
+    // Orientation.lockToPortrait();
+  };
 
   render() {
     return <Navigator />;
