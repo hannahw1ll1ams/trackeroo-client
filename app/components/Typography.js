1ff147875ae7bdc6cf1e7f033c3084026383c319
Thanh <tmdoan98@gmail.com>
2019-10-05 15:10:30 +0100

    add color prop

--- app/components/Typography.js
+++ app/components/Typography.js
@@ -2,16 +2,29 @@ import React, { useContext } from "react";
 import { Text } from "react-native";
 import FontContext from "../context/FontContext";
 
+const colors = {
+  primary: "rgba(255,255,255,1)",
+  secondary: "rgba(255,255,255,0.7)",
+  tertiary: "rgba(255,255,255,0.5)"
+};
+
 const fonts = {
   400: "open-sans-regular",
   600: "open-sans-semibold",
   700: "open-sans-bold"
 };
 
-const Typography = ({ fontWeight, ...rest }) => {
+const Typography = ({ fontWeight, color, fontSize, ...rest }) => {
   const hasFontLoaded = useContext(FontContext);
   return hasFontLoaded ? (
-    <Text style={{ fontFamily: fonts[fontWeight] || fonts[400] }} {...rest} />
+    <Text
+      style={{
+        fontSize: fontSize || 14,
+        color: colors[color] || colors.primary,
+        fontFamily: fonts[fontWeight] || fonts[400]
+      }}
+      {...rest}
+    />
   ) : null;
 };
 
