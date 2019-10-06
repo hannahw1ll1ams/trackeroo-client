2562a89884f67c4b17bd57ddc16fbeacbe0d2915
hannahw1ll1ams <hannahw1ll1ams@yahoo.com>
2019-10-06 23:24:27 +0100

    can render map and different runs on map, can see seperate or all together

--- Trackeroo/app/screens/PersonalProfile/PersonalProfileScreen.js
+++ Trackeroo/app/screens/PersonalProfile/PersonalProfileScreen.js
@@ -1,68 +1,74 @@
 import React, { Component } from "react";
-import { FlatList, Platform, StatusBar, StyleSheet } from "react-native";
-import { SafeAreaView } from "react-navigation";
-import { data } from "../../config/fixtures";
-import { ListItem, Icon } from "react-native-elements";
-import ToggleButton from '../../components/ToggleButton'
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1
-  },
-  padLeft: {
-    paddingLeft: 16
-  },
-  padRight: {
-    paddingRight: 16
-  }
-});
+import { SafeAreaView } from "react-navigation";
+import ToggleButton from '../../components/ToggleButton'
+import MapView, { Marker, Polyline } from 'react-native-maps';
+import Constants from 'expo-constants';
+import styles from './styles';
+import { Text, View, Button } from 'react-native';
 
 export class PersonalProfileScreen extends Component {
-  static navigationOptions = {
-    headerLeft: (
-      <Icon
-        name={`${Platform.OS === "ios" ? "ios" : "md"}-menu`}
-        type="ionicon"
-        color="white"
-        containerStyle={styles.padLeft}
-      />
-    ),
-    headerTitle: "List",
-    headerRight: (
-      <React.Fragment>
-        <Icon
-          name={`${Platform.OS === "ios" ? "ios" : "md"}-chatbubbles`}
-          type="ionicon"
-          color="white"
-          containerStyle={styles.padRight}
-        />
-        <Icon
-          name={`${Platform.OS === "ios" ? "ios" : "md"}-more`}
-          type="ionicon"
-          color="white"
-          containerStyle={styles.padRight}
-        />
-      </React.Fragment>
-    )
-  };
 
-  keyExtractor = item => String(item.id);
+  state = {
+    isMapTrue: false,
+    showSpecificRun: false,
+    specificRun: '',
+    pastRuns: [
+      [{ latitude: 53.192014, longitude: -2.895863 }, { latitude: 53.191860, longitude: -2.895772 }, { latitude: 53.191799, longitude: -2.895954 }, { latitude: 53.191941, longitude: -2.896062 }, { latitude: 53.191899, longitude: -2.896346 }, { latitude: 53.191867, longitude: -2.896635 }, { latitude: 53.191876, longitude: -2.896807 }],
 
-  renderItem = ({ item }) => (
-    <ListItem
-      title={`${item.firstName} ${item.lastName}`}
-      subtitle={item.job}
-      leftAvatar={{ source: { uri: item.avatar } }}
-      bottomDivider={true}
-    />
-  );
+      [{ latitude: 53.192240, longitude: -2.896228 }, { latitude: 53.192223, longitude: -2.896571 }, { latitude: 53.192211, longitude: -2.896850 }, { latitude: 53.192420, longitude: -2.896915 }, { latitude: 53.192516, longitude: -2.896700 }, { latitude: 53.192397, longitude: -2.896657 }, { latitude: 53.192342, longitude: -2.897178 }]]
+  }
+
+  onMapLayout = () => {
+    this.setState({ isMapTrue: true })
+  }
+
+  onPress = (event, runNumber) => {
+    console.log(event, runNumber)
+    if (runNumber === 'all') {
+      this.setState({ showSpecificRun: false, specificRun: '' })
+    }
+    else {
+      this.setState({ showSpecificRun: true, specificRun: runNumber })
+    }
+  }
 
   render() {
+    const { navigation } = this.props;
+    const { pastRuns, showSpecificRun, specificRun, isMapTrue } = this.state;
     return (
-      <SafeAreaView style={styles.container}>
-        <StatusBar backgroundColor="#3b5998" barStyle="light-content" />
-        <FlatList data={data} keyExtractor={this.keyExtractor} renderItem={this.renderItem} />
-        <ToggleButton style={styles.button} navigation={this.props.navigation} />
+      <SafeAreaView style={styles.wholeContainer}>
+        <ToggleButton style={styles.button} navigation={navigation} />
+        <MapView onLayout={this.onMapLayout}
+          style={styles.map}
+          showsUserLocation
+          followsUserLocation
+          initialRegion={{
+            latitude: 53.190959,
+            longitude: -2.864260,
+            latitudeDelta: 0.02,
+            longitudeDelta: 0.02
+          }}>
+          {showSpecificRun === false && isMapTrue &&
+            <View>
+              <Polyline coordinates={[{ latitude: 53.192014, longitude: -2.895863 }, { latitude: 53.191860, longitude: -2.895772 }, { latitude: 53.191799, longitude: -2.895954 }, { latitude: 53.191941, longitude: -2.896062 }, { latitude: 53.191899, longitude: -2.896346 }, { latitude: 53.191867, longitude: -2.896635 }, { latitude: 53.191876, longitude: -2.896807 }]} strokeWidth={5}
+              />
+              <Polyline coordinates={[{ latitude: 53.192240, longitude: -2.896228 }, { latitude: 53.192223, longitude: -2.896571 }, { latitude: 53.192211, longitude: -2.896850 }, { latitude: 53.192420, longitude: -2.896915 }, { latitude: 53.192516, longitude: -2.896700 }, { latitude: 53.192397, longitude: -2.896657 }, { latitude: 53.192342, longitude: -2.897178 }]} strokeWidth={5}
+              />
+            </View>
+          }
+          {showSpecificRun === true && isMapTrue && specificRun === 'run1' &&
+            <Polyline coordinates={[{ latitude: 53.192014, longitude: -2.895863 }, { latitude: 53.191860, longitude: -2.895772 }, { latitude: 53.191799, longitude: -2.895954 }, { latitude: 53.191941, longitude: -2.896062 }, { latitude: 53.191899, longitude: -2.896346 }, { latitude: 53.191867, longitude: -2.896635 }, { latitude: 53.191876, longitude: -2.896807 }]} strokeWidth={5}
+            />
+          }
+          {showSpecificRun === true && isMapTrue && specificRun === 'run2' &&
+            <Polyline coordinates={[{ latitude: 53.192240, longitude: -2.896228 }, { latitude: 53.192223, longitude: -2.896571 }, { latitude: 53.192211, longitude: -2.896850 }, { latitude: 53.192420, longitude: -2.896915 }, { latitude: 53.192516, longitude: -2.896700 }, { latitude: 53.192397, longitude: -2.896657 }, { latitude: 53.192342, longitude: -2.897178 }]} strokeWidth={5}
+            />
+          }
+        </MapView>
+        <Button title='RUN 1' onPress={(event) => this.onPress(event, 'run1')} />
+        <Button title='RUN 2' onPress={(event) => this.onPress(event, 'run2')} />
+        <Button title='View all' onPress={(event) => this.onPress(event, 'all')} />
 
       </SafeAreaView>
     );
@@ -70,3 +76,18 @@ export class PersonalProfileScreen extends Component {
 }
 
 export default PersonalProfileScreen;
+
+// const styles = StyleSheet.create({
+//   container: {
+//     flex: 1,
+//     alignItems: 'center',
+//     justifyContent: 'center',
+//     paddingTop: Constants.statusBarHeight,
+//     backgroundColor: '#ecf0f1',
+//   },
+//   paragraph: {
+//     margin: 24,
+//     fontSize: 18,
+//     textAlign: 'center',
+//   },
+// });
