0282700e336b5c9bc03c96feddaf69abb117b3fc
hannahw1ll1ams <hannahw1ll1ams@yahoo.com>
2019-10-06 19:10:19 +0100

    rendered some hard coded rewards, and adding functionality to add a new reward

--- Trackeroo/app/screens/Rewards/styles.js
+++ Trackeroo/app/screens/Rewards/styles.js
@@ -2,9 +2,13 @@ import { StyleSheet } from 'react-native';
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
-    justifyContent: "center",
-    alignItems: "center"
+    flex: 1
+  },
+  padLeft: {
+    paddingLeft: 16
+  },
+  padRight: {
+    paddingRight: 16
   }
 });
 
