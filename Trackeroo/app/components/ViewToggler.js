102348c8153a53722472cb51145e2e792c7593d6
hannahw1ll1ams <hannahw1ll1ams@yahoo.com>
2019-10-06 19:09:29 +0100

    made view toggler reusuable

--- Trackeroo/app/components/ViewToggler.js
+++ Trackeroo/app/components/ViewToggler.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import { View, Text, Button } from 'react-native';
 import GroupCreator from '../components/GroupCreator'
+import RewardCreator from '../components/RewardCreator'
 
 class ViewToggler extends Component {
   state = {
@@ -18,12 +19,13 @@ class ViewToggler extends Component {
 
   render() {
     const { isShowingForm, messageToggle } = this.state;
-    const { postNewGroup } = this.props;
+    const { postNewGroup, item, postNewReward } = this.props;
     return (
       <View>
-        <Button onPress={this.handlePress} title={messageToggle === true ? "+Add Group" : "Hide Form"}>
+        <Button onPress={this.handlePress} title={messageToggle === true ? `+Add${item}` : "Hide Form"}>
         </Button>
-        {(isShowingForm) && <GroupCreator postNewGroup={postNewGroup} />}
+        {(isShowingForm) && (item === 'group') && <GroupCreator postNewGroup={postNewGroup} />}
+        {(isShowingForm) && (item === 'reward') && <RewardCreator postNewReward={postNewReward} />}
       </View>
     );
   }
