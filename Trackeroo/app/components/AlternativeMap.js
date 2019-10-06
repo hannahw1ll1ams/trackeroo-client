2db2137eedf37328893db1f2accde8551e827b9a
hannahw1ll1ams <hannahw1ll1ams@yahoo.com>
2019-10-06 12:23:49 +0100

    rendering a map and marker with starting position, some basic layout for map

--- Trackeroo/app/components/AlternativeMap.js
+++ Trackeroo/app/components/AlternativeMap.js
@@ -3,7 +3,7 @@ import { Platform, Text, View, StyleSheet, TouchableHighlight } from 'react-nati
 import Constants from 'expo-constants';
 import * as Location from 'expo-location';
 import * as Permissions from 'expo-permissions';
-import MapView from 'react-native-maps';
+import MapView, { Marker, Polyline } from 'react-native-maps';
 import RunInfo from './RunInfo';
 import RunInfoNumeric from './RunInfoNumeric';
 import haversine from 'haversine';
@@ -14,6 +14,8 @@ import styles from '../screens/MapView/SharedStyles'
 export default class App extends Component {
   state = {
     errorMessage: null,
+    startTime: {},
+    endTime: {},
     markers: [],
     watchID: null,
     isMapTrue: false
@@ -39,6 +41,12 @@ export default class App extends Component {
     this.setState({ isMapTrue: true })
   }
 
+  onEndRun = () => {
+    const { markers, startTime, endTime } = this.state
+    //send to database, the run route, the start time and end time.
+  }
+
+
   render() {
     const { markers } = this.state
     let text = 'Waiting..';
@@ -49,56 +57,40 @@ export default class App extends Component {
     }
     console.log(markers)
     return (
-      // <View style={styles.container}>
-      //   {markers.map(marker => {
-      //     return <Text>{marker.coords.longitude}</Text>
-      //   })}
-      // </View>
       <View style={styles.container}>
+        {/* {markers.map(marker => {
+          return <Text key={marker.timestamp}>{marker.coords.longitude}</Text>
+        })} */}
         <MapView
           onLayout={this.onMapLayout}
           style={styles.map}
           showsUserLocation
           followsUserLocation
           initialRegion={{
-            latitude: 37.33307,
-            longitude: -122.0324,
+            latitude: 53.190959,
+            longitude: -2.864260,
             latitudeDelta: 0.02,
             longitudeDelta: 0.02
           }}
         >
-          {this.state.isMapTrue &&
+          {markers.length > 0 && <Marker coordinate={{ latitude: markers[0].coords.latitude, longitude: markers[0].coords.longitude }}
+            title="starting position"
+            description={markers[0].description} />}
+
+          {/* {markers.map(marker => (
+            <Marker key={marker.timestamp}
+              coordinate={{ latitude: marker.coords.latitude, longitude: marker.coords.longitude }}
+              title="starting position"
+              description={marker.description}
+            />
+          ))} */}
+          {/* {this.state.isMapTrue &&
             <MapView.Polyline
-              coordinates={this.state.markers.map(marker => marker.coordinate)}
+              coordinates={this.state.markers.map(marker => [{ latitude: marker.coords.latitude, longitude: marker.coords.longitude }])}
               strokeWidth={5}
             />
-          }
+          } */}
         </MapView>
-        <View style={styles.ic1}>
-        </View>
-        <View style={styles.infoWrapper}>
-          <RunInfoNumeric
-            title="Distance"
-            unit="mi"
-            ref={info => (this.distanceInfo = info)}
-          />
-          <RunInfoNumeric
-            title="Speed"
-            unit="0 km/h"
-            ref={info => (this.speedInfo = info)}
-          />
-          <RunInfo
-            title="Direction"
-            value="NE"
-            ref={info => (this.directionInfo = info)}
-          />
-        </View>
-        <View>
-          {/* <Button
-            title="Go to Details"
-            onPress={() => this.props.navigation.navigate('StopWatch')}
-          /> */}
-        </View>
       </View>
     );
   }
