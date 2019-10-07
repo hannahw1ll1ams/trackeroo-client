cc6dfaaa6b0f0aa130997a374c72b37ab3458855
hannahw1ll1ams <hannahw1ll1ams@yahoo.com>
2019-10-07 16:34:20 +0100

    changing data that comes back into calculating totalDistance and average speed

--- app/components/AlternativeMap.js
+++ app/components/AlternativeMap.js
@@ -7,26 +7,43 @@ import MapView, { Marker, Polyline } from 'react-native-maps';
 import RunInfo from './RunInfo';
 import RunInfoNumeric from './RunInfoNumeric';
 import haversine from 'haversine';
-import styles from '../screens/MapView/SharedStyles'
+import styles from '../screens/MapView/SharedStyles';
+import pick from 'lodash.pick';
+
 
 ///this doesn't quite work yet, need to pass in markers correctly to polyline 
 
 export default class AlternativeMap extends Component {
   state = {
     errorMessage: null,
-    runs: [{ endTime: "00:00:00:457", distance: '', speed: '', markers: [] }],
-    ownRunMarkers: [],
-    finishedRun: false,
-    watchID: null,
-    isMapTrue: false
+    ownRunObjects: [],
+    // watchID: null,
+    isMapTrue: false,
+    // routeCoordinates: [],
+    distanceTravelled: 0,
+    prevLatLng: {},
+    currentSpeed: 0,
+    allSpeeds: []
   };
 
-  componentDidMount() {
-    this._getLocationAsync();
+
+  //still tracking when stopped though!!!
+  componentDidUpdate(prevProps) {
+    const { isRunning } = this.props
+    console.log(isRunning, '<---- is running')
+    if (prevProps.isRunning !== this.props.isRunning) {
+      if (isRunning) {
+        this._getLocationAsync();
+      }
+      // if(isRunning === false) {
+      //   const { allSpeeds, distanceTravelled} = this.state;
+      //   const averageSpeed = calcAveSpeed(allSpeeds)
+      //   //SEND POST REQUEST HERE
+      // }
+    }
   }
 
   _getLocationAsync = async () => {
-    const { isRunning } = this.props;
     let { status } = await Permissions.askAsync(Permissions.LOCATION);
     if (status !== 'granted') {
       this.setState({
@@ -34,30 +51,61 @@ export default class AlternativeMap extends Component {
       });
     }
     await Location.watchPositionAsync({ enableHighAccuracy: true, timeInterval: 1000, distanceInterval: 0.1 }, location => {
-      // if (isRunning) {
-      this.setState({ ownRunMarkers: [...this.state.ownRunMarkers, location] })
-      // }
+
+      const { routeCoordinates, distanceTravelled } = this.state
+      const newLatLngs = { latitude: location.coords.latitude, longitude: location.coords.longitude }
+      // const positionLatLngs = pick(location.coords, ['latitude', 'longitude'])
+      // const singleSpeed = pick(location.coords.speed, ['speed'])
+
+      const speed = location.coords.speed
+
+      this.setState({
+        // routeCoordinates: routeCoordinates.concat(positionLatLngs),
+        ownRunObjects: [...this.state.ownRunObjects, location],
+        distanceTravelled: distanceTravelled + this.calcDistance(newLatLngs),
+        prevLatLng: newLatLngs,
+        currentSpeed: speed,
+        allSpeeds: [...this.state.allSpeeds, speed],
+      })
     })
   }
+
+  calcDistance(newLatLng) {
+    const { prevLatLng } = this.state
+    return (haversine(prevLatLng, newLatLng) || 0)
+  }
+
+  calcAveSpeed(allSpeeds) {
+    return nums.reduce((a, b) => (a + b)) / nums.length;
+  }
+
 
   onMapLayout = () => {
     this.setState({ isMapTrue: true })
   }
 
-  onEndRun = (time) => {
-    const { ownRunMarkers } = this.state
-    this.setState({ finishedRun: true })
-  }
+  // onEndRun = (time) => {
+  //   const { ownRunObjects } = this.state
+  //   this.setState({ finishedRun: true })
+  // }
 
   render() {
-    const { ownRunMarkers } = this.state
+    const { ownRunObjects, routeCoordinates, distanceTravelled, prevLatLng, currentSpeed, allSpeeds } = this.state
     let text = 'Waiting..';
     if (this.state.errorMessage) {
       text = this.state.errorMessage;
     } else if (this.state.location) {
       text = JSON.stringify(this.state.location);
     }
-    // console.log(ownRunMarkers)
+    // console.log(ownRunObjects, '<--- routeCoordinates')
+    // // console.log(routeCoordinates, '<--- routeCoordinates')
+    // console.log(distanceTravelled, '<--- distanceTravelled')
+    // console.log(prevLatLng, '<--- prevLatLng')
+    // console.log(currentSpeed, '<--- currentSpeed')
+    console.log(allSpeeds, '<--- allSpeeds')
+
+
+
     return (
       <View style={styles.container}>
         {/* {markers.map(marker => {
@@ -66,39 +114,66 @@ export default class AlternativeMap extends Component {
         <MapView
           onLayout={this.onMapLayout}
           style={styles.map}
-          showsUserLocation
-          followsUserLocation
+          showsUserLocation={true}
+          followsUserLocation={true}
           initialRegion={{
-            latitude: 53.190959,
-            longitude: -2.864260,
+            latitude: 53.486653,
+            longitude: -2.240088,
             latitudeDelta: 0.02,
             longitudeDelta: 0.02
           }}
+        // overlays={[{
+        //   coordinates: this.state.routeCoordinates,
+        //   strokeColor: '#19B5FE',
+        //   lineWidth: 10,
+        // }]}
         >
-          {ownRunMarkers.length > 0 && <Marker coordinate={{ latitude: ownRunMarkers[0].coords.latitude, longitude: ownRunMarkers[0].coords.longitude }}
+          {ownRunObjects.length > 0 && <Marker coordinate={{ latitude: ownRunObjects[0].coords.latitude, longitude: ownRunObjects[0].coords.longitude }}
             title="starting position"
-            description={ownRunMarkers[0].description} />}
+            description={ownRunObjects[0].description} />}
 
-          {/* {markers.map(marker => (
+          {ownRunObjects.map(marker => (
             <Marker key={marker.timestamp}
               coordinate={{ latitude: marker.coords.latitude, longitude: marker.coords.longitude }}
-              title="starting position"
+              title="route"
+              description={marker.description}
+            />
+          ))}
+
+          {/* {routeCoordinates.length > 0 && <Marker coordinate={{ latitude: routeCoordinates[0].latitude, longitude: routeCoordinates[0].longitude }}
+            title="starting position"
+            description={routeCoordinates[0].description} />} */}
+
+          {/* {routeCoordinates.map(marker => (
+            <Marker key={marker.timestamp}
+              coordinate={{ latitude: marker.coords.latitude, longitude: marker.coords.longitude }}
+              title="route"
               description={marker.description}
             />
           ))} */}
 
-          {
-            this.state.isMapTrue && ownRunMarkers.length > 6 &&
+
+
+          {/* {this.state.isMapTrue &&
+            <Polyline strokeWidth={5} coordinates={ownRunObjects.map(marker => {
+              [{ latitude: marker.coords.latitude, longitude: marker.coords.longitude }]
+            })} />
+
+          } */}
+          {/* {
+            this.state.isMapTrue && ownRunObjects.length > 6 &&
             <Polyline coordinates={[
-              { latitude: ownRunMarkers[0].coords.latitude, longitude: ownRunMarkers[0].coords.longitude },
-              { latitude: ownRunMarkers[1].coords.latitude, longitude: ownRunMarkers[1].coords.longitude },
-              { latitude: ownRunMarkers[2].coords.latitude, longitude: ownRunMarkers[2].coords.longitude },
-              { latitude: ownRunMarkers[3].coords.latitude, longitude: ownRunMarkers[3].coords.longitude },
-              { latitude: ownRunMarkers[4].coords.latitude, longitude: ownRunMarkers[4].coords.longitude }
+              { latitude: ownRunObjects[0].coords.latitude, longitude: ownRunObjects[0].coords.longitude },
+              { latitude: ownRunObjects[1].coords.latitude, longitude: ownRunObjects[1].coords.longitude },
+              { latitude: ownRunObjects[2].coords.latitude, longitude: ownRunObjects[2].coords.longitude },
+              { latitude: ownRunObjects[3].coords.latitude, longitude: ownRunObjects[3].coords.longitude },
+              { latitude: ownRunObjects[4].coords.latitude, longitude: ownRunObjects[4].coords.longitude }
             ]} strokeWidth={5}
             />
-          }
+          } */}
         </MapView>
+        <Text>Speed: {currentSpeed} </Text>
+        <Text>Distance: {distanceTravelled} km</Text>
       </View>
     );
   }
--- app/screens/Run/RunScreen.js
+++ app/screens/Run/RunScreen.js
@@ -2,6 +2,9 @@ import React, { useState } from "react";
 import { View, Button } from "react-native";
 import Typography from "../../components/Typography";
 import * as api from "../../api";
+import AlternativeMap from '../../components/AlternativeMap'
+import AlternativeStopWatch from '../../components/AlternativeStopWatch'
+import styles from '../MapView/SharedStyles'
 
 const RunScreen = () => {
   const [isRunning, setIsRunning] = useState(false);
@@ -10,13 +13,16 @@ const RunScreen = () => {
     //make request here
   };
 
+  updateActivityStatus = (boolean, time) => {
+    console.log(time)
+    setIsRunning(boolean)
+  }
+
   return (
     <View>
       <Typography>Hello</Typography>
-      <Button
-        title={isRunning ? "Stop" : "Start"}
-        onPress={() => setIsRunning(!isRunning)}
-      />
+      <AlternativeMap isRunning={isRunning} />
+      <AlternativeStopWatch style={styles.stopwatch} updateActivityStatus={updateActivityStatus} />
     </View>
   );
 };
