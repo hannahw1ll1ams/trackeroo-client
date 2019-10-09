a58a55101a1452eda5e12d6019e3d43bb61f5f1b
john rowan <johnrowan@johns-MacBook-Air.local>
2019-10-09 17:38:09 +0100

    added styling to login screen

--- app/screens/MapView/MapScreen.js
+++ app/screens/MapView/MapScreen.js
@@ -3,28 +3,27 @@ import React, { Component } from 'react';
 import { View, Text, Button } from 'react-native';
 // import Map from '../../components/Map';
 // import StopWatch from '../../components/StopWatch';
-import AlternativeMap from '../../components/AlternativeMap'
+import AlternativeMap from '../../components/AlternativeMap';
 import AlternativeStopWatch from '../../components/AlternativeStopWatch';
 
 class MapScreen extends Component {
-
   state = {
     isRunning: false,
     endTime: '',
     peopleRunning: ['John'],
     joiningRun: false
-  }
+  };
 
   updateActivityStatus = (boolean, endTime) => {
     this.setState({ isRunning: boolean, endTime }, () => {
-      console.log(this.state.isRunning, '<--updatedState')
-      console.log(this.state.endTime, '<--endTime')
-    })
-  }
+      console.log(this.state.isRunning, '<--updatedState');
+      console.log(this.state.endTime, '<--endTime');
+    });
+  };
 
   toggleStopWatch = () => {
-    this.setState({ joiningRun: !this.state.joiningRun })
-  }
+    this.setState({ joiningRun: !this.state.joiningRun });
+  };
 
   render() {
     const { isRunning, peopleRunning } = this.state;
@@ -32,23 +31,40 @@ class MapScreen extends Component {
       <View style={styles.wholeContainer}>
         <AlternativeMap isRunning={isRunning} />
 
-        {peopleRunning.length === 0 && <Text style={styles.text}>Start a run?</Text>}
-        {peopleRunning.length === 0 && <Button title={!this.state.joiningRun ? 'START' : 'HIDE'} onPress={this.toggleStopWatch} />}
+        {peopleRunning.length === 0 && (
+          <Text style={styles.text}>Start a run?</Text>
+        )}
+        {peopleRunning.length === 0 && (
+          <Button
+            title={!this.state.joiningRun ? 'START' : 'HIDE'}
+            onPress={this.toggleStopWatch}
+          />
+        )}
 
-
-        {peopleRunning.length > 0 && <Text style={styles.text}>Currently Running:</Text>}
+        {peopleRunning.length > 0 && (
+          <Text style={styles.text}>Currently Running:</Text>
+        )}
         {peopleRunning.length > 0 &&
           peopleRunning.map(person => {
-            return <Text key={person}>{person}</Text>
-          })
-        }
-        {peopleRunning.length > 0 && <Text style={styles.statsBox}>JOHN's LIVE DISTANCE, TIME, SPEED</Text>}
+            return <Text key={person}>{person}</Text>;
+          })}
+        {peopleRunning.length > 0 && (
+          <Text style={styles.statsBox}>JOHN's LIVE DISTANCE, TIME, SPEED</Text>
+        )}
 
-        {peopleRunning.length > 0 &&
-          <Button title={!this.state.joiningRun ? 'JOIN' : 'HIDE'} onPress={this.toggleStopWatch} />}
+        {peopleRunning.length > 0 && (
+          <Button
+            title={!this.state.joiningRun ? 'JOIN' : 'HIDE'}
+            onPress={this.toggleStopWatch}
+          />
+        )}
 
-        {this.state.joiningRun === true && <AlternativeStopWatch style={styles.stopwatch} updateActivityStatus={this.updateActivityStatus} />}
-
+        {this.state.joiningRun === true && (
+          <AlternativeStopWatch
+            style={styles.stopwatch}
+            updateActivityStatus={this.updateActivityStatus}
+          />
+        )}
       </View>
     );
   }
